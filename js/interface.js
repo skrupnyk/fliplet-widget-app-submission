ea1007efb5b7c0b71e46d5083111c68911297424
Nicholas Valbusa <squallstar@gmail.com>
2017-05-10 13:51:24 +0200

    files upload

--- js/interface.js
+++ js/interface.js
@@ -3,6 +3,14 @@ var $submissions = $('[data-submissions]');
 var $form = $('[data-submission]');
 var currentSubmission;
 var submissionType;
+var uploadedFiles = {};
+
+$('input[type="file"]').each(function () {
+  $(this).parent().append([
+    '<p class="message hidden alert alert-info"></p>',
+    '<ul class="files"></ul>'
+  ].join(''));
+});
 
 $('[data-create]').click(function (event) {
   event.preventDefault();
@@ -31,6 +39,65 @@ $('[data-submissions]').change(function () {
   });
 });
 
+$('input[type="file"]').change(function () {
+  var formData = new FormData();
+  var files = this.files;
+  var $field = $(this);
+  var fieldName = $field.attr('name');
+  var $message = $field.parent().find('.message');
+  var currentFilesCount = $field.parent().find('.files li').length;
+  var max = parseInt($field.data('max') || 99);
+
+  $message.removeClass('hidden');
+
+  if (currentFilesCount >= max) {
+    event.preventDefault();
+    $message.text('You can only upload ' + max + ' files for this field.');
+    return;
+  }
+
+  for (var i = 0; i < files.length; i++) {
+    formData.append(fieldName, files.item(i));
+  }
+
+  $field.val('');
+  $message.text('Uploading ' + (files.length+1) + ' files...');
+
+  Fliplet.Media.Files.upload({
+    data: formData
+  }).then(function (files) {
+    $message.addClass('hidden');
+    $message.text('');
+
+    files.forEach(function (file) {
+      addFile($field, file);
+    });
+  }).catch(function (err) {
+    $message.html(err.message || err.description || err);
+  });
+});
+
+function addFile($field, file) {
+  var $ul = $field.parent().find('.files');
+  var $li = $([
+    '<li data-id="' + file.id + '">',
+    '<img src="' + file.thumbnail + '" />',
+    '<a data-delete href="#">Delete</a>',
+    '</li>'
+  ].join(''));
+
+  uploadedFiles[file.id] = file;
+  $ul.append($li);
+}
+
+$('.files').on('click', '[data-delete]', function (event) {
+  event.preventDefault();
+  var $li = $(this).parent();
+  $li.remove();
+
+  Fliplet.Media.Files.delete($li.data('id'));
+});
+
 $('input[name=submissionType]').change(function(){
     //submissionType = this.value;
     showForm();
@@ -40,6 +107,7 @@ function hideForm() {
   currentSubmission = undefined;
   $form.addClass('hidden');
   $form[0].reset();
+  $form.find('.files').html('');
 }
 
 function showForm() {
@@ -63,19 +131,30 @@ function showForm() {
 
 function fillForm() {
   _.forIn(currentSubmission.data, function (value, key) {
-    
     var $element = $form.find('[name="' + key + '"]');
-
-    if ($element.attr('type') === 'radio') {
-      $form.find('[name="' + key + '"][value="' + value + '"]').prop("checked", true);
-    } else {
-      $element.val(value);
-    }
 
     if (key === 'submissionType' && value !== '') {
       submissionType = value;
-      alert(submissionType);
     }
+
+    if ($element.attr('type') === 'file') {
+      if (Array.isArray(value)) {
+        value.forEach(function (file) {
+          addFile($element, file);
+        });
+      } else {
+        console.warn('Files for field ' + key + ' are expected to be an array.')
+      }
+
+      return;
+    }
+
+    if ($element.attr('type') === 'radio') {
+      $form.find('[name="' + key + '"][value="' + value + '"]').prop("checked", true);
+      return;
+    }
+
+    $element.val(value);
   });
 
   $form.find('[data-save]').toggleClass('hidden', ['started', 'failed'].indexOf(currentSubmission.status) === -1);
@@ -127,6 +206,18 @@ function saveForm() {
     obj[item.name] = item.value;
     return obj;
   }, {});
+
+  $('input[type="file"]').each(function () {
+    var $el = $(this);
+    var $files = $el.parent().find('.files li');
+    var filesList = [];
+
+    $files.each(function () {
+      filesList.push(uploadedFiles[$(this).data('id')]);
+    });
+
+    data[$el.attr('name')] = filesList;
+  });
 
   return Fliplet.App.Submissions.update(currentSubmission.id, data).catch(function (err) {
     alert(err.responseJSON.message);
