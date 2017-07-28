b7a8f4e4ddaf80875cc04d946c52e979f6de72c8
hcarneiro <hcarneiro@weboo.biz>
2017-07-28 16:24:21 +0100

    Get updated submission and extend

--- js/interface.js
+++ js/interface.js
@@ -278,44 +278,55 @@ function submissionBuild(appSubmission, origin) {
 
 function save(origin, submission) {
 
-  if (submission.status !== 'started') {
-    return Fliplet.App.Submissions.create({
-        platform: 'ios',
-        data: submission.data,
-        result: submission.result
-      })
-      .then(function(newSubmission) {
-        if (origin === "appStore") {
-          appStoreSubmission = newSubmission;
-        }
-        if (origin === "enterprise") {
-          enterpriseSubmission = newSubmission;
-        }
-        if (origin === "unsigned") {
-          unsignedSubmission = newSubmission;
-        }
-
-        Fliplet.App.Submissions.update(newSubmission.id, newSubmission.data).then(function() {
-          $('.save-' + origin + '-progress').addClass('saved');
-
-          setTimeout(function() {
-            $('.save-' + origin + '-progress').removeClass('saved');
-          }, 4000);
-        });
-
-      })
-      .catch(function(err) {
-        alert(err.responseJSON.message);
+  Fliplet.App.Submissions.get()
+    .then(function(submissions) {
+      var savedSubmission = _.find(submissions, function(sub) {
+        return sub.id === submission.id;
       });
-  }
 
-  Fliplet.App.Submissions.update(submission.id, submission.data).then(function() {
-    $('.save-' + origin + '-progress').addClass('saved');
+      submission = _.extend(savedSubmission, submission);
+      return Promise.resolve();
+    })
+    .then(function() {
+      if (submission.status !== 'started') {
+        return Fliplet.App.Submissions.create({
+            platform: 'ios',
+            data: submission.data,
+            result: submission.result
+          })
+          .then(function(newSubmission) {
+            if (origin === "appStore") {
+              appStoreSubmission = newSubmission;
+            }
+            if (origin === "enterprise") {
+              enterpriseSubmission = newSubmission;
+            }
+            if (origin === "unsigned") {
+              unsignedSubmission = newSubmission;
+            }
 
-    setTimeout(function() {
-      $('.save-' + origin + '-progress').removeClass('saved');
-    }, 4000);
-  });
+            Fliplet.App.Submissions.update(newSubmission.id, newSubmission.data).then(function() {
+              $('.save-' + origin + '-progress').addClass('saved');
+
+              setTimeout(function() {
+                $('.save-' + origin + '-progress').removeClass('saved');
+              }, 4000);
+            });
+
+          });
+      }
+
+      Fliplet.App.Submissions.update(submission.id, submission.data).then(function() {
+        $('.save-' + origin + '-progress').addClass('saved');
+
+        setTimeout(function() {
+          $('.save-' + origin + '-progress').removeClass('saved');
+        }, 4000);
+      });
+    })
+    .catch(function(err) {
+      alert(err.responseJSON.message);
+    });
 }
 
 function requestBuild(origin, submission) {
