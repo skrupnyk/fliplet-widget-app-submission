41d5bd49e4777bcb0b7d7323252d49c4c2fbb25b
Nicholas Valbusa <squallstar@gmail.com>
2018-11-22 18:46:36 +0100

    check for undefined var

--- js/interface.js
+++ js/interface.js
@@ -888,7 +888,7 @@ function saveAppStoreData(request) {
 
     if (name === 'fl-store-distribution') {
       var newValue = $('[name="'+name+'"]:checked').val();
-      if (newValue === 'previous-file') {
+      if (newValue === 'previous-file' && appStorePreviousCredential) {
         pushData.apnTeamId = appStorePreviousCredential.teamId;
       }
       if (newValue === 'generate-file' || newValue === 'upload-file') {
@@ -1767,7 +1767,7 @@ $('#enterpriseConfiguration').validator().on('submit', function(event) {
           return;
         }
 
-        saveEnterpriseData(true);        
+        saveEnterpriseData(true);
       });
     } else {
       Fliplet.Modal.alert({
