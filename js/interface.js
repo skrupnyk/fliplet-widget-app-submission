c701ce9e9aca4713afd12dc35e52621b6ec4abf8
Tony Wu <twu@fliplet.com>
2020-04-30 23:41:45 +0100

    Track app launch via Studio

--- js/interface.js
+++ js/interface.js
@@ -699,6 +699,10 @@ function submissionBuild(appSubmission, origin) {
     }
 
     Fliplet.Studio.emit('refresh-app-submissions');
+    Fliplet.Studio.emit('app-launch', {
+      platform: 'ios',
+      submissionType: origin
+    })
 
     Fliplet.Modal.alert({
       title: 'Your request was sent successfully!',
