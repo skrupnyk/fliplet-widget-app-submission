27fd654c460f3480f83db33c5274fc1f27d37bc6
Tony Wu <tonytlwu@users.noreply.github.com>
2021-08-12 19:38:27 +0100

    Include all save buttons when disabling form

--- js/interface.js
+++ js/interface.js
@@ -2308,14 +2308,14 @@ function setFirebaseStatus(credentialKey, origin) {
 
 function disableForm() {
   $(formInputSelectors.join(',')).prop('disabled', true);
-  $('[data-push-save], [data-app-store-save], [data-app-store-build], [change-bundleid]')
+  $('[data-push-save], [data-app-store-save], [data-enterprise-save], [data-unsigned-save], [data-app-store-build], [change-bundleid]')
     .addClass('disabled')
     .prop('disabled', true);
 }
 
 function enableForm() {
   $(formInputSelectors.join(',')).prop('disabled', false);
-  $('[data-push-save], [data-app-store-save], [data-app-store-build], [change-bundleid]')
+  $('[data-push-save], [data-app-store-save], [data-enterprise-save], [data-unsigned-save], [data-app-store-build], [change-bundleid]')
     .removeClass('disabled')
     .prop('disabled', false);
 }
