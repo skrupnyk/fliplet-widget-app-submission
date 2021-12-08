33f82357e55ef1303e943c784d4b361c6959f13f
skrupnyk <serhii.kr@upplabs.com>
2021-12-08 08:47:05 +0200

    update version number validation

--- js/interface.js
+++ js/interface.js
@@ -332,6 +332,7 @@ function loadAppStoreData() {
     if (name === 'fl-store-versionNumber') {
       if (typeof appStoreSubmission.data[name] !== 'undefined' && appStoreSubmission.data[name] !== '') {
         $('[name="' + name + '"]').val(appStoreSubmission.data[name]);
+        $('[name="fl-store-versionNumber"]').data('validation-version-number', appStoreSubmission.data[name]);
       } else if (typeof appStoreSubmission.previousResults !== 'undefined' && typeof appStoreSubmission.previousResults.versionNumber !== 'undefined' && appStoreSubmission.previousResults.versionNumber !== '') {
         $('[name="' + name + '"]').val(appStoreSubmission.previousResults.versionNumber);
         $('[name="fl-store-versionNumber"]').data('validation-version-number', appStoreSubmission.previousResults.versionNumber);
@@ -549,6 +550,7 @@ function loadEnterpriseData() {
     if (name === 'fl-ent-versionNumber') {
       if (typeof enterpriseSubmission.data[name] !== 'undefined' && enterpriseSubmission.data[name] !== '') {
         $('[name="' + name + '"]').val(enterpriseSubmission.data[name]);
+        $('[name="fl-ent-versionNumber"]').data('validation-version-number', enterpriseSubmission.data[name]);
       } else if (typeof enterpriseSubmission.previousResults !== 'undefined' && typeof enterpriseSubmission.previousResults.versionNumber !== 'undefined' && enterpriseSubmission.previousResults.versionNumber !== '') {
         $('[name="' + name + '"]').val(enterpriseSubmission.previousResults.versionNumber);
         $('[name="fl-ent-versionNumber"]').data('validation-version-number', enterpriseSubmission.previousResults.versionNumber);
@@ -707,6 +709,7 @@ function loadUnsignedData() {
     if (name === 'fl-uns-versionNumber') {
       if (typeof unsignedSubmission.data[name] !== 'undefined' && unsignedSubmission.data[name] !== '') {
         $('[name="' + name + '"]').val(unsignedSubmission.data[name]);
+        $('[name="fl-uns-versionNumber"]').data('validation-version-number', unsignedSubmission.data[name]);
       } else if (typeof unsignedSubmission.previousResults !== 'undefined' && typeof unsignedSubmission.previousResults.versionNumber !== 'undefined' && unsignedSubmission.previousResults.versionNumber !== '') {
         $('[name="' + name + '"]').val(unsignedSubmission.previousResults.versionNumber);
         $('[name="fl-ent-versionNumber"]').data('validation-version-number', unsignedSubmission.previousResults.versionNumber);
