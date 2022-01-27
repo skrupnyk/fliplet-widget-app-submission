9e305738bbf6878dc5b799d743045c99902dd25a
skrupnyk <serhii.kr@upplabs.com>
2022-01-27 15:58:59 +0200

    refactor

--- js/interface.js
+++ js/interface.js
@@ -1977,9 +1977,14 @@ function validateScreenshots() {
   return true;
 }
 
-function removeImageErrors(imageSelector, errorSelector) {
+function hideErrors(imageSelector, errorSelector) {
   imageSelector.removeClass('has-error');
   errorSelector.addClass('hidden');
+}
+
+function showErrors(imageSelector, errorSelector) {
+  imageSelector.addClass('has-error');
+  errorSelector.removeClass('hidden');
 }
 
 function validateImageUrl(url, imageSelector, errorSelector) {
@@ -1990,12 +1995,11 @@ function validateImageUrl(url, imageSelector, errorSelector) {
     img.onerror = reject;
     img.src = url;
   }).then(function() {
-    removeImageErrors(imageSelector, errorSelector);
+    hideErrors(imageSelector, errorSelector);
 
     return;
   }).catch(function() {
-    imageSelector.addClass('has-error');
-    errorSelector.removeClass('hidden');
+    showErrors(imageSelector, errorSelector);
   });
 }
 
